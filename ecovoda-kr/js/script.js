5a9fcf2d187d372a4a36894a3c3c7b6048ff0b7b
Sania Basii <basyanya@ukr.net>
2016-05-10 08:07:04 +0300

    slide-3 for md,sm,xs; navbar-menu

--- ecovoda-kr/js/script.js
+++ ecovoda-kr/js/script.js
@@ -351,9 +351,9 @@ jQuery(document).ready(function ($) {
     var dd = today.getDate();
     var mm = today.getMonth()+1; //January is 0!
     var yyyy = today.getFullYear();
-    if(dd<10) {    dd='0'+dd} 
-    if(mm<10) {    mm='0'+mm} 
-    today = mm+'.'+dd+'.'+yyyy;
+    if(dd<10) {    dd = '0'+dd;} 
+    if(mm<10) {    mm = '0'+mm;} 
+    today = dd+'.'+mm+'.'+yyyy;
      $('#mydatepicker .input-group.date').datepicker({
             format: "DD - dd MM yyyy",
             startDate: today,
