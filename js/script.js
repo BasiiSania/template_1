ddfa253e13554c63760ad1974f9b65c4af9d325a
Okolo Basii <oleksandr.basii@gmail.com>
2016-08-09 15:06:19 +0300

    smally corractshny

--- js/script.js
+++ js/script.js
@@ -1,46 +1,5 @@
 
 $(document).ready(function(e) {
-//	$('.with-hover-text, .regular-link').click(function(e){
-//		e.stopPropagation();
-//	});
-//	
-//	/***************
-//	* = Hover text *
-//	* Hover text for the last slide
-//	***************/
-//	$('.with-hover-text').hover(
-//		function(e) {
-//			$(this).css('overflow', 'visible');
-//			$(this).find('.hover-text')
-//				.show()
-//				.css('opacity', 0)
-//				.delay(200)
-//				.animate(
-//					{
-//						paddingTop: '25px',
-//						opacity: 1
-//					},
-//					'fast',
-//					'linear'
-//				);
-//		},
-//		function(e) {
-//			var obj = $(this);
-//			$(this).find('.hover-text')
-//				.animate(
-//					{
-//						paddingTop: '0',
-//						opacity: 0
-//					},
-//					'fast',
-//					'linear',
-//					function() {
-//						$(this).hide();
-//						$( obj ).css('overflow', 'hidden');
-//					}
-//				);
-//		}
-//	);
 	
 	var img_loaded = 0;
 	var j_images = [];
@@ -88,44 +47,6 @@ $(document).ready(function(e) {
 	});
 	
 });
-
-/******************
-* = Gallery width *
-*****************
-$(function() {
-	var pause = 50; // will only process code within delay(function() { ... }) every 100ms.
-	$(window).resize(function() {
-		delay(function() {
-				var gallery_images = $('#slide-3 img');
-				
-				var images_per_row = 0;
-				if ( gallery_images.length % 2 == 0 ) {
-					images_per_row = gallery_images.length / 2;
-				} else {
-					images_per_row = gallery_images.length / 2 + 1;
-				}
-				
-				var gallery_width = $('#slide-3 img').width() * $('#slide-3 img').length;
-				gallery_width /= 2;
-				if ( $('#slide-3 img').length % 2 != 0 ) {
-					gallery_width += $('#slide-3 img').width();
-				}
-				
-				$('#slide-3 .row').css('width', gallery_width );
-				
-				var left_pos = $('#slide-3 .row').width() - $('body').width();
-				left_pos /= -2;
-				
-				$('#slide-3 .row').css('left', left_pos);
-			
-			},
-			pause
-		);
-	});
-	$(window).resize();
-});
-*/
-
 
 var delay = (function(){
 	var timer = 0;
