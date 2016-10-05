e14b4c5999d3aa3622f99f9ff66d543c5dbba765
Okolo Basii <oleksandr.basii@gmail.com>
2016-10-05 21:18:04 +0300

    corractions

--- js/script.js
+++ js/script.js
@@ -166,70 +166,6 @@ jQuery(document).ready(function ($) {
 	});
 });
 
-/***************
-* = Menu hover *
-***************
-jQuery(document).ready(function ($) {
-	//Cache some variables
-	var menu_item = $('.nav').find('li');
-	
-	menu_item.hover(
-		function(e) {
-			var icon = $(this).find('.icon');
-			
-			var left_pos = icon.offset().left - $('.nav').offset().left;
-			var el_width = $(this).find('.text').width();
-			
-			var hover_bar = $('<div class="active-menu special-active-menu"></div>')
-				.css('left', left_pos)
-				.css('width', el_width)
-				.attr('id', 'special-active-menu-' + $(this).data('slide') );
-			
-			$('.active-menu').after( hover_bar );
-		},
-		function(e) {
-			$('.special-active-menu').remove();
-		}
-	);
-});
-*/
-
-/******************
-* = Gallery hover *
-******************/
-//jQuery(document).ready(function ($) {
-//	//Cache some variables
-//	var images = $('#slide-3 a');
-//	
-//	images.hover(
-//		function(e) {
-//			var asta = $(this).find('img');
-//			$('#slide-3 img').not( asta ).stop(false, false).animate(
-//				{
-//					opacity: .5
-//				},
-//				'fast',
-//				'linear'
-//			);
-//			var zoom = $('<div class="zoom"></div>');
-//			if ( $(this).hasClass('video') ) {
-//				zoom.addClass('video');
-//			}
-//			$(this).prepend(zoom);
-//		},
-//		function(e) {
-//			$('#slide-3 img').stop(false, false).animate(
-//				{
-//					opacity: 1
-//				},
-//				'fast',
-//				'linear'
-//			);
-//			$('.zoom').remove();
-//		}
-//	);
-//});
-
 /******************
 * = Arrows click  *
 ******************/
@@ -379,7 +315,8 @@ $('#mydatepicker .help-text-selector').click(function(){
             if (slctdday.getDay()==6) { break; }    }    
         here=true;}
     if (here==true) {
-        if (slctdday.getDay()==7){slctdday.setDate(slctdday.getDate()+1);}
+        if (slctdday.getDay()==0) // IF slctdday is Sunday
+        {slctdday.setDate(slctdday.getDate()+1);}
         $('#mydatepicker .input-group.date').datepicker('setDate', slctdday);}
     else {
         $(this).parent().find('input').attr('value',  this_txt);}
